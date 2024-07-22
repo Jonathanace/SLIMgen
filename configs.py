e4f0f814366b1cb7b359429eca57cb2790f1f787
Jonathanace <jonathanace@gmail.com>
2024-07-22 13:28:20 -0700

    Add additional configs to support ollama directly

--- configs.py
+++ configs.py
@@ -8,3 +8,33 @@ local_llm_config={
     ],
     "cache_seed": None # Turns off caching, useful for testing different models
 }
+
+llama_groq_config = {
+    "config_list": [
+        {
+            "model": "llama3-groq-tool-use",
+            "base_url": "http://localhost:11434/v1",
+            "api_key": "ollama",
+        }
+    ]
+}
+
+codellama_config = {
+    "config_list": [
+        {
+            "model": "codellama",
+            "base_url": "http://localhost:11434/v1",
+            "api_key": "codellama",
+        }
+    ]
+}
+
+llama_instruct_config = {
+    "config_list": [
+        {
+            "model": "llama3:8b-instruct-q6_K",
+            "base_url": "http://localhost:11434/v1",
+            "api_key": "llama3:8b-instruct-q6_K",
+        }
+    ]
+}
