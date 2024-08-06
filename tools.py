c9c9e086f7742d8d7dfe6d4361f823202c3c8899
Jonathanace <jonathanace@gmail.com>
2024-08-06 11:17:16 -0700

    Implement check_readme and walk_directory functions

--- tools.py
+++ tools.py
@@ -1,6 +1,6 @@
 import os
 from autogen import ConversableAgent, register_function
-from configs import llama_groq_config
+from configs import llama_groq_config, llama_3_1_config
 import pprint
 from typing_extensions import Annotated
 from pathlib import Path
@@ -8,8 +8,16 @@ from pathlib import Path
 global project
 project = None
 
+### Agents
+tool_executor = ConversableAgent(
+        name="Tool_Executor",
+        human_input_mode="NEVER",
+        llm_config=False,
+        is_termination_msg=True
+    )
+
 ### Tools
-def read_code(file_path: str) -> str:
+def read_code(file_path: Annotated[str, "File or path to read code from"]) -> str:
     file_path = Path(file_path)
     if not project:
         print('No project directory set.')
@@ -29,7 +37,7 @@ def set_project(
     try:
         project = Path(project_path)
         print(f'Project path set to {project}')
-        return str(project) + "is now set as the project directory. What else can I do for you today?"
+        return str(project) + " is now set as the project directory."
     except Exception as exception:
         return 'Exception!' + str(exception)
     
@@ -47,7 +55,7 @@ def save_code(
     new_file_path.write_text(code)
     return f'The contents of {file_path} are now {read_code(file_path)}'
 
-def check_readme():
+def check_readme()->bool:
     global project
     if not project:
         return 'Project not set. Please enter your desired project path first.'
@@ -56,14 +64,13 @@ def check_readme():
     else:
         return 'A README file doesn\'t exist in the project directory'
 
-
-
+def walk_directory()->str:
+    global project
+    # for file in project.rglob('*'):
+    #     print(read_code(file))
+    return '\n'.join([read_code(file) for file in project.rglob('*')])
 
 ### Tests
-def test_nested_chat():
-    return
-
-
 def test_read_code():
     # Let's first define the assistant agent that suggests tool calls.
     assistant = ConversableAgent(
@@ -74,9 +81,12 @@ def test_read_code():
         "To set the path for the project call set_project(desired_path). "
         "You can save or edit code for a specified file or file path but you **MUST** use the save_code tool to do so."
         "Call check_readme() to check if the project has a README file."
+        "Call walk_directory to output all files in a directory."
         "Return 'TERMINATE' when the task is done.",
         llm_config=llama_groq_config,
-        max_consecutive_auto_reply=1
+        # max_consecutive_auto_reply=1,
+        human_input_mode="NEVER",
+        code_execution_config=False
     )
 
     # The user proxy agent is used for interacting with the assistant agent
@@ -84,8 +94,10 @@ def test_read_code():
     user_proxy = ConversableAgent(
         name="User",
         llm_config=False,
-        is_termination_msg=lambda msg: msg.get("content") is not None and "TERMINATE" in msg["content"],
-        human_input_mode="ALWAYS",
+        is_termination_msg=lambda msg: msg.get("content") is not None and ("TERMINATE" in msg["content"] or "USER INPUT:" in msg["content"]),
+        human_input_mode="TERMINATE",
+        code_execution_config=False,
+        max_consecutive_auto_reply=1
     )
 
     # Register the tool signature with the assistant agent.
@@ -126,9 +138,28 @@ def test_read_code():
         description="A simple tool to checks if a README file exists."
     )
 
-    chat_result = assistant.initiate_chat(user_proxy, message="Please enter your project's relative path:", clear_history=True, silent=False)
-    return
-    
+    register_function(
+        walk_directory,
+        caller=assistant,
+        executor=user_proxy,
+        name="walk_directory",
+        description="Outputs all files in the project directory"
+    )
+
+    ### Nested chats
+    # tool_executor.register_nested_chats(
+    #     trigger=user_proxy,
+    #     chat_queue=[
+    #         {
+    #             "sender": user_proxy,
+    #             "recipient": tool_executor,
+    #             "summary_method": "last_msg",
+    #         }
+    #     ]
+    # )
+    # print(assistant.llm_config["tools"])
+    chat_result = assistant.initiate_chat(user_proxy, message="Please enter your project's relative path. USER INPUT:", clear_history=True, silent=False)
+    # return
 
 if __name__ == '__main__':
     test_read_code()
