46efa1724203fc855a89057d263a03d939d5ca59
Jonathanace <jonathanace@gmail.com>
2024-09-06 15:31:49 -0700

    Refactor files for new src layout

--- agents/__init__.py
+++
@@ -1,4 +0,0 @@
-from .requirement_agent import requirement_writer
-from .sourcecode_agent import source_code_writer
-from .test_writer_agent import test_writer
-from .tool_executor import tool_executor
--- src/slimgen/__main__.py
+++ src/slimgen/__main__.py
@@ -1,6 +1,13 @@
 import os
 import sys
 
+
+if not __package__:
+    # Make CLI runnable from source tree with
+    #    python src/package
+    package_source_path = os.path.dirname(os.path.dirname(__file__))
+    sys.path.insert(0, package_source_path)
+
 if __name__ == "__main__":
     from slimgen.cli import app
     app()
