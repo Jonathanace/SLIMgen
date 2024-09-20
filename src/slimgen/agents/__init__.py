9b079500822031c956dcd91364e445d16a259393
Jonathanace <jonathanace@gmail.com>
2024-09-20 13:54:50 -0700

    Add teachable agent to agents submodule

--- src/slimgen/agents/__init__.py
+++ src/slimgen/agents/__init__.py
@@ -1,4 +1,5 @@
 from agents.requirement_agent import requirement_writer
 from agents.sourcecode_agent import source_code_writer
 from agents.test_writer_agent import test_writer
+from agents.teachable_agent import train_teachable_agent
 # from agents.tool_executor import tool_executor
